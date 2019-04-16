7342f13df26cd5acf3e4a388c32d0e5d800fc266
Oliver Sturm <oliver@oliversturm.com>
2019-04-16 11:50:35 +0100

    demo starting point

---
+++ RoutingEditingValidation/start/Demo/Demo/Shared/Trophy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Shared
+{
+  public enum CoolFactor
+  {
+    Mediocre,
+    PrettyFly,
+    Fierce,
+    MrT
+  }
+
+  public class Trophy
+  {
+    public int Id { get; set; }
+    [Required(AllowEmptyStrings=false, ErrorMessage="Please enter a name")]
+    public string Name { get; set; }
+    public string Source { get; set; }
+    public CoolFactor CoolFactor { get; set; } = CoolFactor.Mediocre;
+  }
+}
