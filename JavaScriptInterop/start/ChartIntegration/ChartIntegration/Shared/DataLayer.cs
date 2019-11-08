0be90396e437551878d179330e33a7dbb8f21d1e
Oliver Sturm <oliver@oliversturm.com>
2019-11-08 16:46:52 +0000

    http

--- JavaScriptInterop/start/ChartIntegration/ChartIntegration/Shared/DataLayer.cs
+++ JavaScriptInterop/start/ChartIntegration/ChartIntegration/Shared/DataLayer.cs
@@ -23,7 +23,7 @@ namespace ChartIntegration.Shared
 
     public async Task<Country[]> FetchCountries(string sortField, bool sortDesc)
     {
-      var url = $"https://outlier.oliversturm.com:8081/countries?sort[0][selector]={sortField}&sort[0][desc]={sortDesc}&take=10";
+      var url = $"http://outlier.oliversturm.com:8080/countries?sort[0][selector]={sortField}&sort[0][desc]={sortDesc}&take=10";
       var data = await httpClient.GetJsonAsync<Data>(url);
       return data.data;
     }
